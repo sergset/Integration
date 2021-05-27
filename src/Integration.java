1310826e1eef5fbfe77ff9dc8dd25f7ad7f7cf72
sergset <45772288+sergset@users.noreply.github.com>
2021-05-27 22:17:37 +0200

    Modified file reading.

--- src/Integration.java
+++ src/Integration.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,13 +10,57 @@ public class Integration extends Methods {
         Scanner sc = new Scanner(file);
         sc.useLocale(Locale.US);
 
-        int n = 11;
-        int tmp = 0;
-        int tmp2 = 0;
-        double x[] = new double[n];
-        double y[] = new double[n];
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        sc.close();
 
-        while (sc.hasNextDouble()) {
+        int temp2=0;
+        int temp3=0;
+        for(int i=0; i<b.length(); i++)
+        {
+            char xd[] = b.toCharArray();
+            if (xd[i]==32)
+            {
+                temp3++;
+            }
+        }
+        double[] x = new double[temp3+1];
+        double[] y = new double[temp3+1];
+        temp3=0;
+        temp2=0;
+        for(int i=0; i<b.length(); i++)
+        {
+            char xd[] = b.toCharArray();
+            String temp;
+            if (xd[i]==32)
+            {
+                temp=b.substring(temp2, i);
+                temp2=i;
+                y[temp3]=Double.parseDouble(temp);
+                temp3++;
+            }
+        }
+        System.out.println(Arrays.toString(y));
+        temp2=0;
+        temp3=0;
+        for(int i=0; i<a.length(); i++)
+        {
+
+            char xd[] = a.toCharArray();
+            String temp;
+            if (xd[i]==32)
+            {
+                temp=a.substring(temp2, i);
+                temp2=i;
+                x[temp3]=Double.parseDouble(temp);
+                temp3++;
+            }
+        }
+        x[temp3]=Double.parseDouble(a.substring(temp2));
+        System.out.println(Arrays.toString(x));
+
+
+        /*while (sc.hasNextDouble()) {
             if (n > tmp) {
                 x[tmp] = sc.nextDouble();
                 tmp++;
@@ -27,8 +72,7 @@ public class Integration extends Methods {
                 y[tmp2] = sc.nextDouble();
                 tmp2++;
             }
-        }
-        sc.close();
+        }*/
 
         Scanner scan = new Scanner(System.in);
         System.out.println(" 1. Prostokątów z niedomiarem\n 2. Prostokątów z nadmiarem\n 3. Trapezów\n 4. Simpsona");
